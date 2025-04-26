c9d5202199410b01104df3ec102902c94b302f46
emmanuel nwafor <emitexc.e.o1@gmail.com>
2025-04-25 18:07:49 -0700

    all commit

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,6 @@
 const NotFound = () => {
     return (
-      <div className="flex-col items-center justify-center flex h-[70vh]">
+      <div className="flex-col items-center justify-center flex h-[40vh]">
         <img src="https://image.shutterstock.com/image-vector/attention-icon-set-danger-caution-260nw-2421574099.jpg" alt="" className="h-20" />
         <h2 className="text-xl font-bold text-center m-10">404 - Page Not Found Site Under Construction</h2>
       </div>
