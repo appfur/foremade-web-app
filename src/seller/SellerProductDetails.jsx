6a2570c54685b8b684bddcaf72e37f7bf9dc9480
emmanuel nwafor <emitexc.e.o1@gmail.com>
2025-05-03 10:29:23 -0700

    all commit

--- src/seller/SellerProductDetails.jsx
+++ src/seller/SellerProductDetails.jsx
@@ -200,7 +200,6 @@ export default function SellerProductDetails() {
     console.log('Editing product:', product.id);
   };
 
-  
   const handleDelete = async (productId) => {
     if (window.confirm('Are you sure you want to delete this product?')) {
       setLoading(true);
